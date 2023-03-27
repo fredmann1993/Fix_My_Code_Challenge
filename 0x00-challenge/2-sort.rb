94de6e5cfc5286b2e3dd7b2171b982e7b5d33298
Fisayo <fasuyifisayo@gmail.com>
2023-03-26 17:11:34 -0700

    committed

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -3,29 +3,28 @@
 #  Sort integer arguments (ascending)
 #
 ###
-
 result = []
 ARGV.each do |arg|
     # skip if not integer
     next if arg !~ /^-?[0-9]+$/
+
     # convert to integer
     i_arg = arg.to_i
+    
     # insert result at the right position
-    result << i_arg
-end
-
-n = result.length
-loop do
-    swapped = false
-
-    (n-1).times do |i|
-        if result[i] < result[i+1]
-            result[i], result[i+1] = result[i+1], result[i]
-            swapped = true
+    is_inserted = false
+    i = 0
+    l = result.size
+    while !is_inserted && i < l do
+        if result[i] < i_arg
+            i += 1
+        else
+            result.insert(i, i_arg)
+            is_inserted = true
+            break
         end
     end
-
-    break if not swapped
+    result << i_arg if !is_inserted
 end
 
 puts result
